57a291c78941f2ccaf52dae8d1e62ab5bed6c9e7
Russel Amin Kadra P. Hanapi <92841378+russelhanapi@users.noreply.github.com>
2025-03-05 21:38:52 +0800

    chore: setup Jest and Babel for testing and Webpack
    - Installed Jest and Babel dependencies
    - Configured `babel.config.js` for ES6+ support
    - Updated Webpack to use `babel-loader` for `.js/.jsx` files
    - Added Jest test script in `package.json`

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/template.html",
+      template: './src/template.html',
     }),
   ],
   module: {
     rules: [
       {
+        test: /\.jsx?$/,
+        exclude: ['node_modules'],
+        use: ['babel-loader'],
+      },
+      {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.html$/i,
-        loader: "html-loader",
+        loader: 'html-loader',
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },
